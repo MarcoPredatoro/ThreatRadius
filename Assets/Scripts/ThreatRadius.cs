dd0dc08c4c049daa05fd6e598c93d08409c6ba04
Michael Wyrley-Birch <an20504@bristol.ac.uk>
2023-03-06 18:11:18 +0000

    Making the sound merge together
    Uses multiple game objects with audio source components and strategically muting them

--- Assets/Scripts/ThreatRadius.cs
+++ Assets/Scripts/ThreatRadius.cs
@@ -11,8 +11,6 @@ public class ThreatRadius : MonoBehaviour
     public GameObject polo2;
     public GameObject background;
 
-    public AudioClip[] threatRadiusSounds;
-
     public float[] threatRadius;
 
 
@@ -30,19 +28,17 @@ public class ThreatRadius : MonoBehaviour
 
         if (dis1 > threatRadius[0] && dis2 > threatRadius[0]) {
             // To far away so should play the background sound
-            polo1.GetComponent<AudioSource>().Stop();
-            polo2.GetComponent<AudioSource>().Stop();
+            SetMute(polo1, new bool[]{true, true, true});
+            SetMute(polo2, new bool[]{true, true, true});
             background.GetComponent<AudioSource>().mute = false;
             Debug.Log("Background Sound");
         } else {
             background.GetComponent<AudioSource>().mute = true;
             if (dis1 < dis2 ) {
-                // polo1.GetComponent<AudioSource>().mute = false;
-                polo2.GetComponent<AudioSource>().Stop();
+                SetMute(polo2, new bool[]{true, true, true});
                 makeThreatRadiusSound(polo1, dis1);
             } else if (dis1 >= dis2) {
-                polo1.GetComponent<AudioSource>().Stop();
-                // polo2.GetComponent<AudioSource>().mute = false;
+                SetMute(polo1, new bool[]{true, true, true});
                 makeThreatRadiusSound(polo2, dis2);
             }
         }
@@ -53,29 +49,30 @@ public class ThreatRadius : MonoBehaviour
         if (distance < threatRadius[2]) {
             // Polo in the closest ring
             Debug.Log("polo in the 3 ring");
-            if (polo.GetComponent<AudioSource>().clip != threatRadiusSounds[2]) {
-                polo.GetComponent<AudioSource>().Stop();
-                polo.GetComponent<AudioSource>().clip = threatRadiusSounds[2];
-                polo.GetComponent<AudioSource>().Play();
+            if (polo.transform.GetChild(2).GetComponent<AudioSource>().mute) {
+                SetMute(polo, new bool[]{false, false, false});
             }
         } else if (distance < threatRadius[1]){
             // Polo in the second closest ring
             Debug.Log("polo in the 2 ring");
-            if (polo.GetComponent<AudioSource>().clip != threatRadiusSounds[1]) {
-                polo.GetComponent<AudioSource>().Stop();
-                polo.GetComponent<AudioSource>().clip = threatRadiusSounds[1];
-                polo.GetComponent<AudioSource>().Play();
+            if (polo.transform.GetChild(1).GetComponent<AudioSource>().mute) {
+                SetMute(polo, new bool[]{false, false, true});
             }
 
         } else {
             // Polo in the furthest ring
             Debug.Log("polo in the 1 ring");
-            if (!polo.GetComponent<AudioSource>().isPlaying || polo.GetComponent<AudioSource>().clip != threatRadiusSounds[0]) {
-                polo.GetComponent<AudioSource>().Stop();
-                polo.GetComponent<AudioSource>().clip = threatRadiusSounds[0];
-                polo.GetComponent<AudioSource>().Play();
+            if (polo.transform.GetChild(0).GetComponent<AudioSource>().mute) {
+                SetMute(polo, new bool[]{false, true, true});
             }
 
+        }
+    }
+
+    void SetMute(GameObject polo, bool[] isMuted) {
+        // Sets whether the child of the polo's audio sources are muted or not based on the bool array
+        for (int i = 0 ; i < isMuted.Length; i++){
+            polo.transform.GetChild(i).GetComponent<AudioSource>().mute = isMuted[i];
         }
     }
 
